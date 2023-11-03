b9e81c3e9d5a9bc73604765a880bf4f8ac9159fe
ccharles123 <cchales123@hotmail.com>
2023-11-03 13:59:30 -0500

    Consumo de la API y Renderizar en las cards

--- Eccomerce-React/src/Pages/Home/index.jsx
+++ Eccomerce-React/src/Pages/Home/index.jsx
@@ -1,12 +1,36 @@
+import { useState, useEffect } from "react";
 import Layout from "../../Components/Layout";
 import Card from "../../Components/Card";
 
+const apiUrl = "https://fakestoreapi.com"
+
 function Home() {
+
+  const [items, setItems] = useState(null)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${apiUrl}/products`)
+        const data = await response.json()
+        setItems(data)
+      } catch (error) {
+        console.error(`Oh no, ocurrió un error: ${error}`);
+      }
+    }
+    fetchData()
+  }, [])
+
+  console.log(items)
 
   return (
       <Layout>
         Home
-        <Card />
+        {
+          items?.map(item => (
+            <Card key={item.id} data={item} />
+          ))
+        }
       </Layout>
   )
 }
