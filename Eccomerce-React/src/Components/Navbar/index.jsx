adfdc6dd270f4f211a8d905448d7a216be4debc8
ccharles123 <cchales123@hotmail.com>
2023-11-11 07:56:29 -0500

    configuracion vite

--- Eccomerce-React/src/Components/Navbar/index.jsx
+++ Eccomerce-React/src/Components/Navbar/index.jsx
@@ -14,7 +14,10 @@ const Navbar = () =>{
         <nav className="flex justify-between items-center fixed z-10 top-0 w-full py-5 px-8 text-sm font-light bg-white">
             <ul className="flex items-center gap-3">
                 <li className="font-semibold text-lg">
-                    <NavLink to= '/'>
+                    <NavLink to= '/'
+                    onClick={()=>setSearchByCategory()}
+                    >
+                        
                         Shopi
                     </NavLink>
                 </li>
